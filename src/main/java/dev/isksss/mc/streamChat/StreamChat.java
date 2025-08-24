78a6cf77e8e963ddb008c1c082f3e2f9101aa61f
isksss <104404522+isksss@users.noreply.github.com>
2025-08-24 17:38:50 +0900

    first commit

---
+++ src/main/java/dev/isksss/mc/streamChat/StreamChat.java
@@ -0,0 +1,17 @@
+package dev.isksss.mc.streamChat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class StreamChat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
